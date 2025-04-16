269e74ba266e6efee26170d4b83ea8d5a1e6ddad
Gris Ge <fge@redhat.com>
2025-04-16 16:53:28 +0800

    Support multi-arch containers now
    Signed-off-by: Gris Ge <fge@redhat.com>

--- push_container.sh
+++ push_container.sh
@@ -1,11 +1,20 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 trap exit INT
 
+echo "Make sure you have \`qemu-user-static\` installed"
+
 TAGS=(nm-c9s nm-c10s libreswan-srv-c9s libreswan-cli-c9s)
 
+
 for TAG in "${TAGS[@]}"; do
-    podman build -f ./Containerfile.$TAG \
-        -t quay.io/nmstate/test-env:$TAG
-    podman push quay.io/nmstate/test-env:$TAG
+    IMAGE="quay.io/nmstate/test-env:$TAG"
+    podman image rm $IMAGE -f || true
+    podman manifest rm $IMAGE || true
+    podman manifest create $IMAGE
+    podman build \
+        --platform linux/amd64,linux/arm64,linux/ppc64le,linux/s390x \
+        -f ./Containerfile.$TAG \
+        --manifest $IMAGE
+    podman manifest push $IMAGE
 done
