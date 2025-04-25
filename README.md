10d0f1f39cfcf32c36f61b994fec2c3d07ec572b
Gris Ge <fge@redhat.com>
2025-04-25 16:49:52 +0800

    ipsec: Introduce `--server-only` argument
    With `--server-only` defined, the script will only create IPSec server
    container leaving NIC `ipsec_cli` in current namespace, will also load
    IPSec PKI keys to current ipsec database.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- README.md
+++ README.md
@@ -1,62 +1,88 @@
-# Containers for test usage
+<!-- vim-markdown-toc GFM -->
+
+* [Containers for test usage](#containers-for-test-usage)
+    * [NetworkManager](#networkmanager)
+        * [NetworkManager on CentOS Stream 10](#networkmanager-on-centos-stream-10)
+        * [NetworkManager on CentOS Stream 9](#networkmanager-on-centos-stream-9)
+    * [IPSec](#ipsec)
+        * [IPSec using PSK authentication](#ipsec-using-psk-authentication)
+        * [IPSec using RSA authentication](#ipsec-using-rsa-authentication)
+        * [IPSec using PKI authentication](#ipsec-using-pki-authentication)
+        * [IPSec P2P connection](#ipsec-p2p-connection)
+        * [IPSec Site-to-Site connection](#ipsec-site-to-site-connection)
+        * [IPSec Transport connection (p2p only)](#ipsec-transport-connection-p2p-only)
+        * [IPSec Host-to-Site connection](#ipsec-host-to-site-connection)
+        * [IPSec IPv4 in IPv6 and IPv6 in IPv4 connection](#ipsec-ipv4-in-ipv6-and-ipv6-in-ipv4-connection)
+
+<!-- vim-markdown-toc -->
 
 The containers are stored in (https://quay.io/repository/nmstate/test-env)
+# Containers for test usage
 
-## NetworkManager on CentOS Stream 10
+## NetworkManager
+
+### NetworkManager on CentOS Stream 10
 
 ```bash
 sudo ./run.sh nm c10s
 ```
 
-## NetworkManager on CentOS Stream 9
+### NetworkManager on CentOS Stream 9
 
 ```bash
 sudo ./run.sh nm c9s
 ```
 
-## IPSec using PSK authentication
+## IPSec
+
+By default, this script will create both server and client containers.
+
+Passing `--server-only` will only create server container and load IPSec
+keys to local ipsec database.
+
+### IPSec using PSK authentication
 
 ```bash
 sudo ./run.sh ipsec psk
 ```
 
-## IPSec using RSA authentication
+### IPSec using RSA authentication
 
 ```bash
 sudo ./run.sh ipsec rsa
 ```
 
-## IPSec using PKI authentication
+### IPSec using PKI authentication
 
 ```bash
 sudo ./run.sh ipsec cert
 ```
 
-## IPSec P2P connection
+### IPSec P2P connection
 
 ```bash
 sudo ./run.sh ipsec p2p
 ```
 
-## IPSec Site-to-Site connection
+### IPSec Site-to-Site connection
 
 ```bash
 sudo ./run.sh ipsec site2site
 ```
 
-## IPSec Transport connection (p2p only)
+### IPSec Transport connection (p2p only)
 
 ```bash
 sudo ./run.sh ipsec transport
 ```
 
-## IPSec Host-to-Site connection
+### IPSec Host-to-Site connection
 
 ```bash
 sudo ./run.sh ipsec host2site
 ```
 
-## IPSec IPv4 in IPv6 and IPv6 in IPv4 connection
+### IPSec IPv4 in IPv6 and IPv6 in IPv4 connection
 
 ```bash
 sudo ./run.sh ipsec mix46
--- ipsec/run.sh
+++ ipsec/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -13,6 +13,8 @@ SRV_CORP_IPV6_ADDR="fd00:a::1"
 
 SRV_NET_NS_NAME="ipsec-srv"
 CLI_NET_NS_NAME="ipsec-cli"
+SRV_NIC_NAME="ipsec_srv"
+CLI_NIC_NAME="ipsec_cli"
 SRV_CONTAINER_NAME="ipsec-srv"
 CLI_CONTAINER_NAME="ipsec-cli"
 
@@ -30,9 +32,12 @@ function pull_container_image {
 function clean_up {
     podman rm -f $CLI_CONTAINER_NAME 1>/dev/null 2>/dev/null || true
     podman rm -f $SRV_CONTAINER_NAME 1>/dev/null 2>/dev/null || true
-    ip netns exec $CLI_NET_NS_NAME ip link del ipsec_cli 2>/dev/null || true
+    ip netns exec $SRV_NET_NS_NAME \
+        ip link del $SRV_NIC_NAME 2>/dev/null || true
     ip netns del $SRV_NET_NS_NAME 2>/dev/null || true
     ip netns del $CLI_NET_NS_NAME 2>/dev/null || true
+    rm -rfv /var/lib/ipsec/nss/*.db || true
+    ipsec initnss || true
 }
 
 trap clean_up EXIT
@@ -41,12 +46,18 @@ function setup_podman_network {
     clean_up
 
     ip netns add $SRV_NET_NS_NAME
-    ip netns add $CLI_NET_NS_NAME
-    ip link add ipsec_cli type veth peer ipsec_srv
-    ip link set ipsec_srv netns $SRV_NET_NS_NAME
-    ip link set ipsec_cli netns $CLI_NET_NS_NAME
-    ip netns exec $SRV_NET_NS_NAME ip link set ipsec_srv up
-    ip netns exec $CLI_NET_NS_NAME ip link set ipsec_cli up
+    if [[ $SRV_ONLY -eq 0 ]]; then
+        ip netns add $CLI_NET_NS_NAME
+    fi
+    ip link add $CLI_NIC_NAME type veth peer $SRV_NIC_NAME
+    ip link set $SRV_NIC_NAME netns $SRV_NET_NS_NAME
+    ip netns exec $SRV_NET_NS_NAME ip link set $SRV_NIC_NAME up
+    if [[ $SRV_ONLY -eq 0 ]]; then
+        ip link set $CLI_NIC_NAME netns $CLI_NET_NS_NAME
+        ip netns exec $CLI_NET_NS_NAME ip link set $CLI_NIC_NAME up
+    else
+        ip link set $CLI_NIC_NAME up
+    fi
 }
 
 function wait_services {
@@ -89,8 +100,17 @@ function start_cli {
     podman run -d --privileged --replace \
         --name $CLI_CONTAINER_NAME --hostname cli-a.example.org \
         --network ns:/run/netns/$CLI_NET_NS_NAME \
+        --network bridge
         $CLI_IMAGE
     wait_services $CLI_CONTAINER_NAME
+}
+
+function load_cli_keys {
+    ipsec checknss;
+    pk12util -i $SCRIPT_DIR/pki/cli-a.example.org.p12 \
+        -d sql:/var/lib/ipsec/nss -W ""
+    certutil -M -n "Easy-RSA CA" -t CT  -d sql:/var/lib/ipsec/nss/
+    systemctl restart ipsec
 }
 
 #### Main function starts
@@ -108,7 +128,7 @@ elif [ "CHK$1" == "CHKcert" ];then
     NMSTATE_IPSEC_CLI_YML="/root/nmstate_conf/nmstate_cert_gw.yml"
 elif [ "CHK$1" == "CHKp2p" ];then
     IPSEC_CONF="libreswan_conf/p2p.conf"
-    NMSTATE_IPSEC_CLI_YML="/root/nmstate_conf/nmstate_p2p_gw.yml"
+    NMSTATE_IPSEC_CLI_YML="/root/nmstate_conf/nmstate_p2p.yml"
 elif [ "CHK$1" == "CHKsite2site" ];then
     IPSEC_CONF="libreswan_conf/site_to_site.conf"
     NMSTATE_IPSEC_CLI_YML="/root/nmstate_conf/nmstate_site_to_site.yml"
@@ -125,29 +145,59 @@ else
     echo "Invalid argument."
     echo -n "Valid are 'psk', 'rsa', 'cert', 'p2p', 'site2site', 'transport', "
     echo "'host2site', 'mix46'."
+    echo "Use --server-only for creating server container only"
     exit 1
+fi
+
+SRV_ONLY=0
+
+if [[ "$@" != *"--server-only"* ]]; then
+    SRV_ONLY=1
 fi
 
 setup_podman_network
 start_srv
 
-start_cli
+
+if [[ $SRV_ONLY -eq 0 ]]; then
+  start_cli
+else
+  load_cli_keys
+  NMSTATE_IPSEC_CLI_YML="$SCRIPT_DIR/nmstate_conf/$(basename \
+      $NMSTATE_IPSEC_CLI_YML)"
+fi
+
 start_ipsec_service
 
-podman exec -i $CLI_CONTAINER_NAME nmstatectl apply $NMSTATE_IPSEC_CLI_YML
+if [[ $SRV_ONLY -eq 0 ]]; then
+    podman exec -i $CLI_CONTAINER_NAME nmstatectl apply $NMSTATE_IPSEC_CLI_YML
 
-echo "
-Both IPSEC Server and Client container started.
+    echo "
+    Both IPSEC Server and Client container started.
 
-You will be placed in the client container. Exit will purge both containters.
+    You will be placed in the client container. Exit will purge both containters.
 
-The nmstate YAML $NMSTATE_IPSEC_CLI_YML has applied.
+    The nmstate YAML $NMSTATE_IPSEC_CLI_YML has applied.
 
-To check the ipsec connection, try
+    To check the ipsec connection, try
 
-    ipsec status
-    ip xfrm state
-    nmcli c
-"
-podman exec -it $CLI_CONTAINER_NAME bash
+        ipsec status
+        ip xfrm state
+        nmcli c
+    "
+    podman exec -it $CLI_CONTAINER_NAME bash
+else
+    echo "
+    IPSEC Server has been started.
+    The client authenticaion key has been imported.
+    You may start the ipsec connection by invoking command(in another terminal):
+
+        nmstatectl apply $SCRIPT_DIR/ipsec_cli_network.yml
+        nmstatectl apply $NMSTATE_IPSEC_CLI_YML
+
+    Press enter to clean and quit.
+    ".
+    read
+fi
+
 echo "Cleaning up"
