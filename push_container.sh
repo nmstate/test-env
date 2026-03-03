d2371dbb3c2372cb444dc2b2024a1caf1d75071c
Gris Ge <fge@redhat.com>
2026-03-03 22:50:20 +0800

    The `push_container.sh` only works in Fedora
    Signed-off-by: Gris Ge <fge@redhat.com>

--- push_container.sh
+++ push_container.sh
@@ -4,6 +4,18 @@ trap exit INT
 
 PROJECT_BASE_DIR=$(readlink -f "$(dirname -- "$0")");
 
+# The podman cross platform is only works in Fedora with `qemu-user-static`,
+# even Archlinux will not work, hence we force user of this script
+# to run in Fedora
+
+if [ ! -f /etc/fedora-release ];then
+    echo "Please run this script in Fedora"
+    exit 1
+fi
+
+rpm -q qemu-user-static 1>/dev/null 2>/dev/null || \
+    (echo "Please install qemu-user-static"; exit 1)
+
 echo "Make sure you have \`qemu-user-static\` installed"
 
 TAGS=(nm-c9s nm-c10s libreswan-srv-c9s libreswan-cli-c9s 8021x-srv-c10s)
